b7db9058193c32d13731b3e3ed725120264587fa
yusank <yusankurban@gmail.com>
2021-11-22 18:53:27 +0800

    fix: add new function

--- datastruct/string.go
+++ datastruct/string.go
@@ -1,21 +1,60 @@
 package datastruct
 
-import (
-	"sync"
-)
+/*
+ * String Command
+ */
 
-type String struct {
-	m  sync.Map
-	mc *MemoryCache
+func Set(key string, value interface{}, options ...string) {
+	defaultCache.keys.Store(key, &KeyInfo{Type: KeyTypeString, Value: value})
 }
 
-func newString(mc *MemoryCache) *String {
-	return &String{
-		m:  sync.Map{},
-		mc: mc,
+func Incr(key string, incrBy float64) (value float64, err error) {
+	v, ok := defaultCache.keys.Load(key)
+	if !ok {
+		Set(key, incrBy)
+		return incrBy, nil
 	}
+
+	info := v.(*KeyInfo)
+	if info.Type != KeyTypeString {
+		return 0, ErrKeyAndCommandNotMatch
+	}
+
+	info.Value = info.Value.(float64) + incrBy
+	return info.Value.(float64), nil
 }
 
-var (
-	StringImpl = newString(defaultCache)
-)
+func Get(key string) (interface{}, error) {
+	v, ok := defaultCache.keys.Load(key)
+	if !ok {
+		return "", ErrNil
+	}
+
+	info := v.(*KeyInfo)
+	if info.Type != KeyTypeString {
+		return "", ErrKeyAndCommandNotMatch
+	}
+
+	return info.Value, nil
+}
+
+func MGet(keys ...string) []interface{} {
+	var result []interface{}
+
+	for _, key := range keys {
+		val, err := Get(key)
+		if err == ErrNil {
+			result = append(result, nil)
+			continue
+		}
+
+		if err != nil {
+			result = append(result, err)
+			continue
+		}
+
+		result = append(result, val)
+	}
+
+	return result
+}
