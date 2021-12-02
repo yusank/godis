dcb26b45e58a3a052c9905fa79c96b778fac1caf
yusank <yusankurban@gmail.com>
2021-12-02 16:48:17 +0800

    feat: support set basic function  #3

--- datastruct/set.go
+++ datastruct/set.go
@@ -5,25 +5,66 @@ import (
 )
 
 type set struct {
-	m sync.Map
+	m      sync.Map
+	length int
 }
 
 func newSet() *set {
 	return &set{m: sync.Map{}}
 }
 
-func loadSet(key string) (*set, error) {
+func (s *set) sAdd(key string) int {
+	_, loaded := s.m.LoadOrStore(key, 0)
+	if loaded {
+		return 0
+	}
+	s.length++
+	return 1
+}
+
+func (s *set) sRem(key string) int {
+	_, loaded := s.m.LoadAndDelete(key)
+	if !loaded {
+		return 0
+	}
+
+	s.length--
+	return 1
+}
+
+func sDiff(s1, s2 *set) []string {
+	var result []string
+	s1.m.Range(func(key, _ interface{}) bool {
+		if _, ok := s2.m.Load(key); !ok {
+			result = append(result, key.(string))
+		}
+
+		return true
+	})
+
+	return result
+}
+
+/*
+ * Commands
+ */
+
+func loadAndCheckSet(key string, check bool) (*set, error) {
 	info, err := loadKeyInfo(key, KeyTypeSortedSet)
 	if err != nil {
 		return nil, err
 	}
 
 	s := info.Value.(*set)
+	if check && s.length == 0 {
+		return nil, ErrNil
+	}
+
 	return s, nil
 }
 
 func SAdd(key string, values ...string) (int, error) {
-	s, err := loadSet(key)
+	s, err := loadAndCheckSet(key, false)
 	if err == ErrNil {
 		s = newSet()
 		err = nil
@@ -35,10 +76,30 @@ func SAdd(key string, values ...string) (int, error) {
 
 	var cnt int
 	for _, value := range values {
-		_, loaded := s.m.LoadOrStore(value, 0)
-		if !loaded {
-			cnt++
-		}
+		cnt += s.sAdd(value)
+	}
+
+	return cnt, nil
+}
+
+func SCard(key string) (int, error) {
+	s, err := loadAndCheckSet(key, true)
+	if err != nil {
+		return 0, err
+	}
+
+	return s.length, nil
+}
+
+func SRem(key string, values ...string) (int, error) {
+	s, err := loadAndCheckSet(key, true)
+	if err != nil {
+		return 0, err
+	}
+
+	var cnt int
+	for _, value := range values {
+		cnt += s.sRem(value)
 	}
 
 	return cnt, nil
